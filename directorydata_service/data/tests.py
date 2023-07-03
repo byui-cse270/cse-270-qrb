e4222dba075ab6b346ec61627abb895e645d29c7
qrbriggs-byui <briggsq@byui.edu>
2023-07-03 17:54:50 -0600

    Empty test cases for users

--- directorydata_service/data/tests.py
+++ directorydata_service/data/tests.py
@@ -1,19 +1,3 @@
-from django.test import TestCase, RequestFactory
-from django.urls import reverse
-from data.views import index
+from django.test import TestCase
 
-class IndexViewTestCase(TestCase):
-    def setUp(self):
-        self.factory = RequestFactory()
-
-    def test_my_view(self):
-        # Create a GET request to the view
-        url = reverse('all')  
-        request = self.factory.get(url)
-
-        # Call the view function with the request
-        response = index.as_view()(request)
-
-        # Perform assertions on the response
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'businesses')
+# Create your tests here.
